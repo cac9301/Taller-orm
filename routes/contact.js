74bc1bcf4210021823eb5c4b189bca430f64303c
cac9301 <cac9301@gmail.com>
2019-03-14 02:38:39 +0000

    True ORM

--- routes/contact.js
+++ routes/contact.js
@@ -1,57 +1,54 @@
-"use strict";
-var express = require("express");
-var Contact = require("../models/contact");
+'use strict';
+var express = require('express');
+var Contact = require('../models/contact');
 var router = express.Router();
-router
-	.route("/")
-	.get((req, res) => {
-		Contact.fetchAll().then(contacts => {
-			res.json(contacts);
-		});
-	})
-router
-	.route("/")
-	.post((req, res) => {
-		new Contact({
-				firstName: req.body.firstName,
-				lastName: req.body.lastName,
-				emailAddress: req.body.emailAddress
-			})
-			.save()
-			.then(saved => {
-				res.json(saved);
-			});
-	});
-router
-	.route("/:id")
-	.put((req, res) => {
-		Contact.where("id", req.params.id)
-			.fetch()
-			.then(contact => {
-				contact
-					.save({
-						firstName: req.body.firstName,
-						lastName: req.body.lastName,
-						emailAddress: req.body.emailAddress
-					})
-					.then(saved => {
-						res.json(saved);
-					});
-			});
-	})
-	.get((req, res) => {
-		Contact.where("id", req.params.id)
-			.fetch()
-			.then(contact => {
-				res.json(contact);
-			});
-	})
-	.delete((req, res) => {
-		Contact.where("id", req.params.id)
-			.destroy()
-			.then(destroyed => {
-				res.json(destroyed);
-			});
-	});
+router.route('/')
+.get(function(req, res) {
+Contact
+.fetchAll()
+.then(function(contacts) {
+res.json({ contacts });
+});
+});
+router.route('/')
+.post(function(req, res) {
+    new Contact({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailAddress: req.body.emailAddress,
+    })
+      .save()
+      .then(function(saved) {
+        res.json({ saved });
+      });
+  });
+  
 
+router.route('/:id')
+  .put(function(req, res) {
+    Contact
+      .where('id', req.params.id)
+      .fetch()
+      .then(function(contact) {
+        contact
+          .save({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            emailAddress: req.body.emailAddress,
+          })
+          .then(function(saved) {
+            res.json({ saved });
+          });
+      });
+  });
+  
+router.route('/:id')
+  .delete(function(req, res) {
+    Contact
+      .where('id', req.params.id)
+      .destroy()
+      .then(function(destroyed) {
+        res.json({ destroyed });
+      });
+  }); 
 module.exports = router;
